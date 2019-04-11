8bb81c91dfd427a488e0c33cbcd9bc3aa5cf06e2
Dan Rogers <dan.rogers@idfive.com>
2019-04-11 14:12:46 -0600

    http://bugify.idfive.com/issues/19731, documentation updates

---
+++ modules/hp_events_feed/src/Plugin/ExternalDataSource/EventSchoolsColleges.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * @file
+ * Provides Drupal\external_data_source\Plugin\ExternalWsSource\EventSchoolsColleges.
+ */
+
+namespace Drupal\hp_events_feed\Plugin\ExternalDataSource;
+
+use Drupal\external_data_source\Plugin\ExternalDataSourceBase;
+use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception as GuzzleException;
+
+/**
+ * Provides a 'Event Schools and Colleges' ExternalDataSource.
+ *
+ * @ExternalDataSource(
+ *   id = "event_schools_colleges",
+ *   name = @Translation("Event Schools and Colleges"),
+ *   description = @Translation("This Plugin will gather a list of Howard Event Schools and Colleges.")
+ * )
+ */
+class EventSchoolsColleges extends ExternalDataSourceBase {
+
+  /**
+   *
+   * @return string
+   */
+  public function getPluginId() {
+    return 'event_schools_colleges';
+  }
+
+  /**
+   *
+   * @return string
+   */
+  public function getPluginDefinition() {
+    return $this->t('This Plugin will gather a list of Howard Event Schools and Colleges.');
+  }
+
+  /**
+   * setRequest
+   * Setting sent request
+   *
+   * @params Symfony\Component\HttpFoundation\Request $request
+   */
+  public function setRequest(Request $request) {
+    $this->request = $request;
+  }
+
+  /**
+   * getRequest
+   * getting sent request
+   *
+   * @return \Symfony\Component\HttpFoundation\Request $request
+   */
+  public function getRequest() {
+    return $this->request;
+  }
+
+  /**
+   * getResponse
+   * Call WS to retrieve data
+   * @return array
+   */
+  public function getResponse() {
+    $cid = 'hp_events_feed_external_data_source_event_schools_colleges';
+    if ($this->request && !is_null($this->request->get('q'))) {
+      $this->q = $this->request->get('q');
+      $cid = $cid . '_' . $this->request->get('q');
+    }
+    $data = [];
+    if ($cache = \Drupal::cache()->get($cid)) {
+      $data = $cache->data;
+    }
+    else {
+      $client = new Client();
+      try {
+        // taxonomy_1 is the admin category endpoint on howard calendar
+        $response = $client->get('https://calendar.howard.edu/api/taxonomy_1/');
+        $data = json_decode($response->getBody()->getContents());
+        $data = $data->data;
+      } catch (GuzzleException $e) {
+        watchdog_exception('external_data_source', $e->getMessage());
+      }
+      //Caching result to avoid ws over use
+      \Drupal::cache()->set($cid, $data);
+    }
+    return $this->formatResponse($data);
+  }
+
+  /**
+   * formatResponse
+   *
+   * @param array $response
+   * Formatting data retrieved from ws to match [{"value":"","label":""},
+   *   {"value":"", "label":""}] return array $collection retrieved suggestions
+   *
+   * @return array $collection
+   */
+  public function formatResponse(array $response) {
+    $collection = [];
+    foreach ($response as $entry) {
+      // Workaround to set as a text string, as a bug prevents from setting simply a number, even as string.
+      $value = 'id=' . strval($entry->id);
+      $collection[] = [
+        'value' => $value,
+        'label' => t($entry->label)
+      ];
+    }
+    return $collection;
+  }
+
+}
--- modules/hp_profiles_feed/README.md
+++ modules/hp_profiles_feed/README.md
@@ -1,13 +1,26 @@
 # Howard Paragraphs Profiles Feed
 
-Paragraphs integration of Profiles Feed (from profiles.howard.edu) for Howard projects:
+Paragraphs integration of a Profiles Feed (from profiles.howard.edu) for Howard projects. Essentially, this module pulls a feed of profiles from profiles.howard.edu, with optional filter paramaters available, in order to feature faculty and staff by department.
 
 This module contains markup only (no js or css), those should be provided in the client theme, loaded via the idfive Component Library:
 
  - [idfive Component Library](https://bitbucket.org/idfivellc/idfive-component-library)
  - [idfive Component Library D8 Theme](https://bitbucket.org/idfivellc/idfive-component-library-d8-theme)
 
-## Markup Overrides
+## Configuration
+The following fields are available on this KS widget:
+ - Title: Sets the title in the header bar. Defaults to empty.
+ - Environment: Choose which environment of the profiles site to pull profiles from. Defaults to production.
+ - Department: Filters profiles by the Department taxonomy (profiles_department) on profilies.howard.edu.
+ - Department Level: Filters frofiles by department level. These levels are pulled from the "Department Level" on each profile.
 
+## Notes on profiles feed filtering
+If no optional filters are chosen, it will show a default feed, though we highly reccomend choosing a department.
+
+These filters are additive, meaning they will combine if you choose more than one. For example, if both "Department" and "Department Level" are chosen, the query would say "Give me all people tagged as department=x AND department_level=y". 
+
+More information and documentation on the profiles API can be found in the idfive_profile_api module on huenterprise.
+
+## Markup Overrides
 - You may override paragraphs templates by copying them into the client theme.
 - You may override hooks by copying into client .theme, and modifying hook name/etc.
