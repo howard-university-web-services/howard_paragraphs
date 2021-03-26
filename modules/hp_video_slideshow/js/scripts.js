f70b067fdc9bd874f5fe91fa08edb99ff4ae74db
Antonio McMichael <antonio.mcmichael@howard.edu>
2021-03-26 01:21:18 -0400

    Address feedback

--- modules/hp_video_slideshow/js/scripts.js
+++ modules/hp_video_slideshow/js/scripts.js
@@ -4,25 +4,63 @@ jQuery(document).ready(function($) {
         timeout: 0,
         pager: '.hp_video_slideshow--slideshow-nav',
         before: function() {
-            this.style.visibility = "visible";
+            $(this).css('visibility', 'visible');
 
             // Load the Vimeo player.
-            let iframe = this.querySelector('iframe');
+            let iframe = $(this).find('iframe');
             let player = new Vimeo.Player(iframe);
 
-            // When the video ends
-            // 1) Reset the iframe.
+            // When the video loads, show caption text if applicable.
+            let caption = $(this).find('.hp_video_slideshow--video-text');
+            player.on('play', function(data) {
+                if (caption != null)
+                {
+                    caption.fadeIn(3000);
+                }
+            });
+
+            // Hide until the video loads.
+            player.on('loaded', function(data) {
+                iframe.fadeIn();
+            });
+
+            // Get the slide show reference.
+            let slideshow = $(this).parent();
+
+            // When the video ends:
+            // 1) Reset the video.
             // 2) Remove the callback listener for the current intance.
-            // 3) Go to the next video.
+            // 3) Hide caption if applicable.
+            // 4) Go to the next video.
             player.on('ended', function(data) {
                 player.unload().then(function() {});
                 player.off('ended');
-                $('.hp_video_slideshow--slideshow').cycle('next');
+
+                if (caption != null)
+                {
+                    caption.fadeOut();
+                }
+
+                slideshow.cycle('next');
             });
 
             // Mute the video then play.
             player.setVolume(0);
-            player.play();
+            player.play().catch(function(error) {
+                switch (error.name) {
+                    case 'PasswordError':
+                        console.log('The video is password-protected.');
+                        break;
+
+                    case 'PrivacyError':
+                        console.log('The video is private');
+                        break;
+
+                    // Hide the iframe and show the fallback image.
+                    console.log('Hiding video.');
+                    iframe.fadeOut();
+                }
+            });
         }
     });
 });
