8581b7403727c73a1152a8a6f1f3a8f149f990b9
Antonio McMichael <antonio.mcmichael@howard.edu>
2021-03-30 01:01:06 -0400

    Optimize video player JavaScript usage and improve style.

--- modules/hp_video_slideshow/js/scripts.js
+++ modules/hp_video_slideshow/js/scripts.js
@@ -1,53 +1,78 @@
-jQuery(document).ready(function($) {
+var videoPlayers = {};
+
+jQuery(document).ready(function($)
+{
+    // Initialize each video player.
+    $('.hp_video_slideshow--video-container').each(function()
+    {
+        // Load the Vimeo player.
+        let iframe = $(this).find('iframe');
+        let player = new Vimeo.Player(iframe);
+
+        // When the video plays:
+        // 1) Hide player until the video starts playing.
+        // 2) Show caption text if applicable.
+        let caption = $(this).find('.hp_video_slideshow--video-text');
+        player.on('play', function(data)
+        {
+            setTimeout(function()
+            {
+                iframe.fadeIn(1000);
+
+                if (caption != null)
+                {
+                    caption.fadeIn(2500);
+                    caption.fadeOut(2500);
+                }
+            },
+
+            // Account for flash right
+            // before the video plays.
+            150);
+        });
+
+        // Get the slide show reference.
+        let slideshow = $(this).parent();
+
+        // When the video ends:
+        // 1) Go to the next video.
+        // 2) Hide the video player for fade effect.
+        player.on('ended', function(data)
+        {
+            slideshow.cycle('next');
+            iframe.fadeOut();
+        });
+
+        // Mute the video.
+        player.setVolume(0);
+
+        // Store the player instance in the video players
+        // dictionary to avoid reinstantiation during transitions
+        // and improve performance.
+        let videoPlayerId = Math.random().toString();
+        $(this).attr('videoPlayerId', videoPlayerId);
+        videoPlayers[videoPlayerId] = player;
+    });
+
+    // Start slideshow.
     $('.hp_video_slideshow--slideshow').cycle({
         fx: 'none',
         timeout: 0,
         pager: '.hp_video_slideshow--slideshow-nav',
-        before: function() {
+        before: function()
+        {
+            // Ensure the current video container is visible.
             $(this).css('visibility', 'visible');
 
             // Load the Vimeo player.
             let iframe = $(this).find('iframe');
-            let player = new Vimeo.Player(iframe);
+            let player = videoPlayers[$(this).attr('videoPlayerId')];
 
-            // When the video loads, show caption text if applicable.
-            let caption = $(this).find('.hp_video_slideshow--video-text');
-            player.on('play', function(data) {
-                if (caption != null)
+            // Play the video.
+            player.play().catch(function(error)
+            {
+                switch (error.name)
                 {
-                    caption.fadeIn(3000);
-                }
-            });
-
-            // Hide until the video loads.
-            player.on('loaded', function(data) {
-                iframe.fadeIn();
-            });
-
-            // Get the slide show reference.
-            let slideshow = $(this).parent();
-
-            // When the video ends:
-            // 1) Reset the video.
-            // 2) Remove the callback listener for the current intance.
-            // 3) Hide caption if applicable.
-            // 4) Go to the next video.
-            player.on('ended', function(data) {
-                player.unload().then(function() {});
-                player.off('ended');
-
-                if (caption != null)
-                {
-                    caption.fadeOut();
-                }
-
-                slideshow.cycle('next');
-            });
-
-            // Mute the video then play.
-            player.setVolume(0);
-            player.play().catch(function(error) {
-                switch (error.name) {
                     case 'PasswordError':
                         console.log('The video is password-protected.');
                         break;
@@ -61,6 +86,27 @@ jQuery(document).ready(function($) {
                     iframe.fadeOut();
                 }
             });
+        },
+        onPagerEvent: function(zeroBasedSlideIndex, slideElement)
+        {
+            let currentVideoPlayerId = $(slideElement).attr('videoPlayerId');
+            let iframe = $(slideElement).find('iframe');
+            let caption = $(slideElement).find('.hp_video_slideshow--video-text');
+
+            // Reset all slides except the current one.
+            for (let videoPlayerId in videoPlayers)
+            {
+                if (videoPlayerId != currentVideoPlayerId)
+                {
+                    videoPlayers[videoPlayerId].unload();
+                    iframe.hide();
+
+                    if (caption != null)
+                    {
+                        caption.hide();
+                    }
+                }
+            }
         }
     });
 });
