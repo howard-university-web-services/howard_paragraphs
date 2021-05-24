138f0172ed6452225aa4aa5d8e73c05155b30053
Dan Rogers <dan.rogers@idfive.com>
2021-05-24 08:50:10 -0600

    Clear external Content

---
+++ src/Controller/HowardExternalContentCacheClear.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\howard_paragraphs\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Controller routines for calendar notification routes.
+ */
+class HowardExternalContentCacheClear extends ControllerBase {
+
+  /**
+   * Gets paragraphs that contain external content feeds.
+   */
+  public function clearExternalContent() {
+    $query = \Drupal::entityQuery('paragraph')
+    ->condition('type', [
+      'hp_twitter_feed',
+      'hp_deadline_feed',
+      'hp_events_feed',
+      'hp_facebook_feed',
+      'hp_instagram_feed',
+      'hp_magazine_feed',
+      'hp_news_feed',
+      'hp_profiles_feed',
+      'hp_program',
+      'hp_programs_feed',
+      'hp_alumni_featured',
+      'hp_alumni_feed',
+    ], 'IN');
+    $pids = $query->execute();
+    $cids = [];
+    foreach ($pids as $key => $pid) {
+      $paragraph = Paragraph::load($pid);
+      $tags = $paragraph->getCacheTags();
+      $cids[] = $tags;
+      Cache::invalidateTags($tags);
+
+    }
+    $message = 'Howard external content feed caches cleared.';
+    \Drupal::logger('howard_paragraphs')->notice($message);
+    return $cids;
+  }
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function bootstrapCacheClear() {
+    $paragraph_cids = $this->clearExternalContent();
+    return [
+      '#theme' => 'external_content_cache_clear',
+      '#cids' => $paragraph_cids,
+    ];
+  }
+
+}
