71e6f378469b814a6d712fc65a9783fa2b0f4f3e
sean.kerins <sean.kerins@idfive.com>
2023-12-21 13:46:28 -0500

    Removed config file from Data Points module

---
+++ modules/hp_giving_feed/src/Plugin/ExternalDataSource/GivingCategories.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * @file
+ * Provides Drupal\external_data_source\Plugin\ExternalWsSource\GivingCategories.
+ */
+
+namespace Drupal\hp_giving_feed\Plugin\ExternalDataSource;
+
+use Drupal\external_data_source\Plugin\ExternalDataSourceBase;
+use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception as GuzzleException;
+
+/**
+ * Provides a 'Giving Categories' ExternalDataSource.
+ *
+ * @ExternalDataSource(
+ *   id = "giving_categories",
+ *   name = @Translation("Giving Categories"),
+ *   description = @Translation("This Plugin will gather a list of Howard Giving Categories.")
+ * )
+ */
+class GivingCategories extends ExternalDataSourceBase {
+
+  /**
+   *
+   * @return string
+   */
+  public function getPluginId() {
+    return 'giving_categories';
+  }
+
+  /**
+   *
+   * @return string
+   */
+  public function getPluginDefinition() {
+    return $this->t('This Plugin will gather a list of Howard Giving Categories.');
+  }
+
+  /**
+   * SetRequest
+   * Setting sent request.
+   *
+   * @params Symfony\Component\HttpFoundation\Request $request
+   */
+  public function setRequest(Request $request) {
+    $this->request = $request;
+  }
+
+  /**
+   * GetRequest
+   * getting sent request.
+   *
+   * @return \Symfony\Component\HttpFoundation\Request $request
+   */
+  public function getRequest() {
+    return $this->request;
+  }
+
+  /**
+   * GetResponse
+   * Call WS to retrieve data.
+   *
+   * @return array
+   */
+  public function getResponse() {
+    $cid = 'hp_giving_feed_external_data_source_giving_categories';
+    if ($this->request && !is_null($this->request->get('q'))) {
+      $this->q = $this->request->get('q');
+      $cid = $cid . '_' . $this->request->get('q');
+    }
+    $data = [];
+    if ($cache = \Drupal::cache()->get($cid)) {
+      $data = $cache->data;
+    }
+    else {
+      $client = new Client();
+      try {
+        // taxonomy_1 is the schools/colleges endpoint on howard newsroom.
+        $response = $client->get('https://giving.howard.edu/jsonapi/taxonomy_term/hc_resource_category', ['verify' => FALSE]);
+        $data = json_decode($response->getBody()->getContents());
+        $data = $data->data;
+      }
+      catch (GuzzleException $e) {
+        watchdog_exception('external_data_source', $e->getMessage());
+      }
+      // Caching result to avoid ws over use.
+      \Drupal::cache()->set($cid, $data);
+    }
+    return $this->formatResponse($data);
+  }
+
+  /**
+   * FormatResponse.
+   *
+   * @param array $response
+   *   Formatting data retrieved from ws to match [{"value":"","label":""},
+   *   {"value":"", "label":""}] return array $collection retrieved suggestions.
+   *
+   * @return array $collection
+   */
+  public function formatResponse(array $response) {
+    $collection = [];
+    foreach ($response as $entry) {
+      // Workaround to set as a text string, as a bug prevents from setting simply a number, even as string.
+      $value = 'id=' . strval($entry->attributes->drupal_internal__tid);
+      $name = $entry->attributes->name;
+      $collection[] = [
+        'value' => $value,
+        'label' => t($name),
+      ];
+    }
+    // \Drupal::logger('hp_ec')->debug('response: ' . json_encode($collection) );
+    return $collection;
+  }
+
+}
